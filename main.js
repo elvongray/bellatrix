21b75d7b39d2dbea756388c7cffed087bcec780f
Arinde Eniola <eniola.arinde@andela.co>
2015-10-10 12:13:16 +0100

    prepare app dependencies

---
+++ main.js
@@ -0,0 +1,27 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+require('crash-reporter').start();
+var debug = process.env.NODE_ENV === 'development';
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+
+  mainWindow = new BrowserWindow({width: 1360, height: 800});
+
+  mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+
+  if(debug) {
+    mainWindow.openDevTools();
+  }
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+
+});
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: ['webpack/hot/dev-server', './app/js/app.js']
+  },
+
+  output: {
+    path: './app/js',
+    filename: 'bundle.js',
+    publicPath: 'http://localhost:8080/app/'
+  },
+
+  devServer: {
+    contentBase: './app',
+    publicPath: 'http://localhost:8080/app/'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: 'style-loader!css-loader' }
+    ]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.IgnorePlugin(new RegExp("^(fs|ipc)$"))
+  ]
+}

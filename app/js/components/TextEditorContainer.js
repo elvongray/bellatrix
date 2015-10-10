ebb8823af918873cf9468bf090f923353699129b
Arinde Eniola <eniola.arinde@andela.co>
2015-10-11 00:48:56 +0100

    tweak ace editor

--- app/js/components/TextEditorContainer.js
+++ app/js/components/TextEditorContainer.js
@@ -11,24 +11,23 @@ var TextEditorContainer = React.createClass({
     var aceEditor = ace.edit("editor");
     aceEditor.setTheme("ace/theme/twilight");
     aceEditor.session.setMode("ace/mode/javascript");
+    aceEditor.setShowPrintMargin(false);
   },
 
   render: function() {
     return (
-      <div>
-        <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-          <header className="mdl-layout__header text-editor-header">
-            <div className="mdl-layout__header-row">
-              <span className="mdl-layout-title">Javascript</span>
-            </div>
-          </header>
-          <main className="mdl-layout__content">
-            <div className="page-content">
-              <pre id="editor">
-              </pre>
-            </div>
-          </main>
-        </div>
+      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <header className="mdl-layout__header text-editor-header">
+          <div className="mdl-layout__header-row">
+            <span className="mdl-layout-title">Javascript</span>
+          </div>
+        </header>
+        <main className="mdl-layout__content">
+          <div className="page-content">
+            <pre id="editor">
+            </pre>
+          </div>
+        </main>
       </div>
     )
   }

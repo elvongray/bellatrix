bf35b17d9d7d7cbfdc75e378b6c655d9ce99d0cd
Arinde Eniola <eniola.arinde@andela.co>
2015-10-10 13:12:58 +0100

    add rendering process

---
+++ app/js/app.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Bellatrix = React.createClass({
+
+  render: function() {
+    return <p>Awesome</p>
+  }
+
+});
+
+React.render(<Bellatrix />, document.getElementById('bellatrix'));

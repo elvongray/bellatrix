a94bf695b26fbf727ed58276a987fb5f9810bfce
Arinde Eniola <eniola.arinde@andela.co>
2015-12-13 12:37:24 +0100

    port the GeneralActions from es5 to es6 and do some refactoring

--- app/js/actions/GeneralActions.js
+++ app/js/actions/GeneralActions.js
@@ -1,14 +1,14 @@
-var fs = window.require('fs');
+const fs = window.require('fs');
 
-var AppDispatcher = require('../dispatcher/AppDispatcher');
+import AppDispatcher from '../dispatcher/AppDispatcher'
 
-var dir = './app/js/stores/';
+let dir = './app/js/stores/'
 
-var GeneralActions = {
+const GeneralActions = {
 
-  loadSavedState: function() {
+  loadSavedState() {
     // Load the saved state of the editor
-    fs.readFile(dir + 'current_state.json', function(err, data) {
+    fs.readFile(dir + 'current_state.json', (err, data) => {
 
       AppDispatcher.dispatch({
         actionType: "LOAD_SAVED_STATE",
@@ -17,28 +17,28 @@ var GeneralActions = {
     })
   },
 
-  saveCurrentState: function(state) {
+  saveCurrentState(state) {
     // Delete the editorText state, not needed for now
     delete state.editorText;
 
     // Save the current state of the editor
-    fs.writeFile(dir + 'current_state.json', JSON.stringify(state), function(err) {
+    fs.writeFile(dir + 'current_state.json', JSON.stringify(state), (err) => {
+      if (err) {
+        console.log("Error writing into file");
+      }
+    });
+  },
+
+  saveEditorText(text, language) {
+    fs.writeFile(`${dir}${language}.txt`, text, (err) => {
       if(err) {
         console.log("Error writing into file");
       }
     });
   },
 
-  saveEditorText: function(text, language) {
-    fs.writeFile(dir + language + ".txt", text, function(err) {
-      if(err) {
-        console.log("Error writing into file");
-      }
-    });
-  },
-
-  loadSavedEditorText: function(language) {
-    fs.readFile(dir + language + ".txt", 'utf8', function(err, data) {
+  loadSavedEditorText(language) {
+    fs.readFile(`${dir}${language}.txt`, 'utf8', (err, data) => {
 
       AppDispatcher.dispatch({
         actionType: "LOAD_SAVED_TEXT",
@@ -48,4 +48,4 @@ var GeneralActions = {
   }
 }
 
-module.exports = GeneralActions;
+export default GeneralActions;
